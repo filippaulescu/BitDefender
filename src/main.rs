09643e7b4d469d6dce4434f1659beddec45200a5
unknown <filip.paulescu04@gmail.com>
2024-04-25 18:30:11 +0300

    Laboratorul 3

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,74 @@
-use std::env;
-use std::fs::{self, File};
-use std::io::{self, BufWriter};
-use std::path::Path;
-use std::io::Write;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-#[derive(Debug, serde::Serialize)]
-struct FileData{
-    name :String,
-    filenames: Vec<String>,
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FileData {
+    /// name of the zip archive
+    name: String,
+    /// list of files in the zip archive
+    files: Vec<String>,
 }
 
-/* 
-fn list_zip_contents<R: io::Read + io::Seek>(
-    reader: R,
-    writer: &mut dyn io::Write,
-) -> zip::result::ZipResult<()> {
-    let mut zip = zip::ZipArchive::new(reader)?;
+use std::collections::HashMap;
+use std::collections::HashSet;
+type Term = String;
+type DocumentID = String;
+type IndexType = HashMap<Term, HashSet<DocumentID>>;
 
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        writeln!(writer, "Filename: {}", file.name())?;
+fn load_data(data_filename: &str) -> Result<Vec<FileData>, Box<dyn std::error::Error>> {
+    let file = File::open(data_filename)?;
+    let reader = BufReader::new(file);
+
+    let mut data: Vec<FileData> = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        let line = line.trim();
+        data.push(serde_json::from_str(line)?);
     }
 
-    Ok(())
-}
-*/
-/*
-fn list_zip_contents(reader: impl Read + Seek) -> zip::result::ZipResult<()> {
-    let mut zip = zip::ZipArchive::new(reader)?;
-
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        println!("Filename: {}", file.name());
-        //std::io::copy(&mut file, &mut std::io::stdout())?;
+    let mut index = IndexType::new(); 
+    for fd in data.iter_mut(){
+        //trebuie split-uit dupa /, am vazut ceva cu .split("/")
+        for term in fd.files.iter_mut(){
+            //println!("Filename: {}\n", entry2.to_string());
+            let x = index.get_mut(term);
+            if let Some(z ) = x {
+                z.insert(fd.name.clone());
+            } else {
+                let mut s = HashSet::new();
+                s.insert(fd.name.clone());
+                index.insert(term.to_string(),  s);
+            }
+        }
     }
 
-    Ok(())
+    Ok(data)
 }
-*/
 
-fn list_zip_contents<R: io::Read + io::Seek>(
-    reader: R
-) -> zip::result::ZipResult<Vec <String>> {
-    let mut zip = zip::ZipArchive::new(reader)?;
-    let mut filenames = Vec:: new();
+fn run_search(data: &IndexType, terms: Vec<&str>){
+    let mut counter: HashMap<DocumentID, u64> = HashMap::new();
+    for term in &terms{
+            if let Some(docs) = data.get(*term){
 
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        filenames.push(file.name().to_string());
+            }else{
+                
+            }
     }
-
-    Ok(filenames)
 }
-
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Obține argumentele din linia de comandă
-    let args: Vec<String> = env::args().collect();
-
-    // Primește calea către director ca primul argument
-    let dir_path = &args[1];
-
-    // Deschide fișierul pentru scriere
-    let output_path = "output.txt";
-    let mut zip_files: Vec<FileData> = Vec::new();
-
-    // Iterează prin fiecare element din director
-    for entry in fs::read_dir(dir_path)? {
-        let entry = entry?;
-        let path = entry.path();
-
-        //INCANTATIE
-        let filename = path
-            .file_name()
-            .unwrap()
-            .to_string_lossy()
-            .to_string();
-
-        zip_files.push(FileData{ 
-            name : filename,
-            filenames: list_zip_contents(fs::File::open(entry.path())?)?
-        });
-    }
-
-    //println!("{:?}",zip_files);
-    let file = File::create(output_path)?;
-    let mut writer = BufWriter::new(file);
-    for i in zip_files.iter() {
-        
-        serde_json::to_writer(&mut writer, &zip_files)?;
-        writer.write_all(&[b'\n'])?;
-        
-    }
-
+    let args: Vec<String> = std::env::args().collect();
+    use std::time::Instant;
+    let start = Instant::now();
+    let data_filename = &args[1];
+    let data = load_data(&data_filename)?;
+    
+    println!("loaded data for {} files", data.len());
+    println!("time {:?} ", start.elapsed());
 
     Ok(())
-
-
-
 }
